a57a52e06be8a32756c3e68b7f83e1faadfe77e1
edwinxp1 <erodriguez.7957@unimar.edu.ve>
2024-11-12 14:52:52 -0400

    Menú y administrador, GestionDeTareas.java
    Añadiendo el menu y panel de administrador, faltaria hacer que las opciones presentes funcionen, ademas de todos los sistemas relacionados con la administracion.

--- GestionDeTareas.java
+++ GestionDeTareas.java
@@ -1,9 +1,59 @@
-package gestiondetareas;
-//Juan Marcano Edwing Rodriguez
-public class GestionDeTareas {
+package proyecto_gestión_de_tareas;
+/*Integrantes:
+Edwin Rodriguez
+Juan Marcano
+*/
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class Proyecto_gestión_de_tareas {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
         
+        do {
+            System.out.println("------------------");
+            System.out.println("| Iniciar sesión |");
+            System.out.println("|----------------|");
+            System.out.println("|1.    Usuario   |");
+            System.out.println("|2. Administrador|");
+            System.out.println("|3.     Salir    |");
+            System.out.println("------------------");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
+            
+            switch(opcion) {
+                case 1:
+                    // Lógica para usuario
+                    System.out.println("Has seleccionado Usuario.");
+                    break;
+                case 2:
+                    System.out.println("Has seleccionado Administrador.");
+                    System.out.flush();
+                    
+                    // Pedir clave de administrador
+                    System.out.print("Ingrese la clave de 4 dígitos: ");
+                    int clave = scanner.nextInt();
+                    
+                    if (clave == 1234) {
+                        
+                        // Mostrar menú de administrador
+                        int opcion1;
+                        do {
+                            System.out.println("----------------------------");
+                            System.out.println("|     Menú Administrador   |");
+                            System.out.println("|--------------------------|");
+                            System.out.println("|1.      Crear usuario     |");
+                            System.out.println("|2.   Eliminar usuario     |");
+                            System.out.println("|3.  Usuarios registrados  |");
+                            System.out.println("|4.  Modificar usuario     |");
+                            System.out.println("|5.        Volver          |");
+                            System.out.println("----------------------------");
+                            System.out.print("Seleccione una opción: ");
+                            opcion1 = scanner.nextInt();
+                            
     }
     
 }
