89979ce2225b490b6656c38a57f7bd5a816d5593
JuanPabloMarcano <jmarcano.1701@unimar.edu.ve>
2024-11-13 21:55:24 -0400

    Modificacion a clase nodo

--- Nodo.java
+++ Nodo.java
@@ -5,11 +5,11 @@ public class Nodo {
     int dato;
     nodo siguiente;
     nodo anterior;
-    nodo(int valor){
-        this.dato = valor;
+    nodo(int tarea){
+        this.dato = tarea;
     }
-    nodo(int valor, nodo anteriorerior){
-        this.dato = valor;
+    nodo(int tarea, nodo anteriorerior){
+        this.dato = tarea;
         this.anterior = anteriorerior;
     }
 }
@@ -24,22 +24,18 @@ public class Nodo {
             aux = L;
             while (aux.siguiente != null)
                 aux= aux.siguiente;
-            /*aux.siguient = new nodo(numero);
-            aux2 = aux;
-            aux = aux.siguient;
-            aux.anterior = aux2;*/
             aux.siguiente = new nodo(numero, aux);
         }
     }
     
-    void eliminar(int valor){
+    void eliminar(int tarea){
         aux=L;
         nodo aux2 = null;
-        while(aux.siguiente!=null || aux.dato != valor){
+        while(aux.siguiente!=null || aux.dato != tarea){
             aux2=aux;
             aux=aux.siguiente;
         }
-        if (aux.dato == valor){
+        if (aux.dato == tarea){
             if (aux.anterior == null) {
                 L = L.siguiente;
                 L.anterior = null;
@@ -47,28 +43,14 @@ public class Nodo {
                 aux = aux.anterior;
                 aux.siguiente = null;
             }else{
-                /*
-                nodo aux3 = aux.siguient;
-                aux2.siguient = aux.siguient;
-                aux3.anterior = aux2;
-                */
                 aux.anterior.siguiente = aux.siguiente;
                 aux.siguiente.anterior = aux.anterior;
                 aux = null;
             }
-            System.out.println(valor + " Eliminado de la lista");
+            System.out.println(tarea + " Eliminado de la lista");
         }else{
-            System.out.println("valor no encontrado en la lista");
+            System.out.println("tarea no encontrado en la lista");
         }
     }
-   void sumar(){
-       aux = L;
-       int sum=0;
-       while (aux.siguiente != null){
-           sum+=aux.dato;
-           aux=aux.siguiente;
-       }
-       System.out.println("La suma de todos los elementos de la lista es: " + sum);
-   }
-}
+  }
 }
